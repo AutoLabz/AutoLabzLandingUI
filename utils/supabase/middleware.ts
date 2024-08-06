bed235e97757f9fd506329653f4aaf58967272ab
Saurabh Khanal <Skhanal247@gmail.com>
2024-08-06 00:40:35 -0400

    Auth Sign In Bug

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -36,6 +36,7 @@ export async function updateSession(request: NextRequest) {
       !user &&
       !request.nextUrl.pathname.startsWith('/login') &&
       !request.nextUrl.pathname.startsWith('/auth') &&
+      !request.nextUrl.pathname.startsWith('/signin') &&
       request.nextUrl.pathname !== '/' // this is to force non automatic redirect
     ) {
       const url = request.nextUrl.clone()
