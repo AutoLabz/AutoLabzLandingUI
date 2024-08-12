4fa53cdf9f1b98abb3953608ec6e7a1ab09205a2
Saurabh Khanal <Skhanal247@gmail.com>
2024-08-12 15:17:28 -0400

    footer changes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,12 +1,78 @@
+import Link from "next/link";
+
 export function Footer() {
     return (
-        <footer className="footer z-10 border-t-[#33353F] border-b-transparent border-l-transparent border-r-transparent text-white">
-          <div className="p-12 flex items-center ">
-            <p className="text-slate-600 ml-auto">
-            &copy; <span className="font-bold">AutoLabz</span> {new Date().getFullYear()}. All rights reserved.
-            </p>
+      <footer>
+      <div className="py-12 md:py-16">
+        <div className="mx-auto max-w-6xl px-4 sm:px-6">
+          <div className="flex flex-col-reverse md:flex-row md:justify-between">
+            {/* Left side - privacy, tos, copyright */}
+            <div className="md:col-span-4 lg:col-span-5">
+              <div className="mb-2 flex items-center">
+                <Link
+                  href="/privacy"
+                  className="ml-3 hover:text-gray-500"
+                  aria-label="Privacy policy"
+                >
+                  Privacy Policy
+                </Link>
+                <Link
+                  href="/terms-of-service"
+                  className="ml-4 hover:text-gray-500"
+                  aria-label="Terms of service"
+                >
+                  Terms of Service
+                </Link>
+              </div>
+              <div className="ml-3 mr-4 mt-4 text-sm text-gray-600 md:mt-0">
+                &copy; AutoLabz - All rights reserved.
+              </div>
+            </div>
+            {/* Right side - social links */}
+            <ul className="mb-4 mt-2 flex md:order-1 md:mb-0 md:ml-4 md:mt-0">
+              <li className="ml-3">
+                <Link
+                  href="#"
+                  target="_blank"
+                  className="flex items-center justify-center text-gray-800 transition duration-150 ease-in-out hover:text-gray-500"
+                  aria-label="Github"
+                >
+                  GitHub
+                </Link>
+              </li>
+              <li className="ml-4">
+                <Link
+                  href="#"
+                  target="_blank"
+                  className="flex items-center justify-center text-gray-800 transition duration-150 ease-in-out hover:text-gray-500"
+                  aria-label="Discord"
+                >
+                  Discord
+                </Link>
+              </li>
+              <li className="ml-4">
+                <Link
+                  href="/changelog"
+                  className="flex items-center justify-center text-gray-800 transition duration-150 ease-in-out hover:text-gray-500"
+                  aria-label="About"
+                >
+                  Changelog
+                </Link>
+              </li>
+              <li className="ml-4">
+                <Link
+                  href="/about"
+                  className="flex items-center justify-center text-gray-800 transition duration-150 ease-in-out hover:text-gray-500"
+                  aria-label="About"
+                >
+                  About
+                </Link>
+              </li>
+            </ul>
           </div>
-        </footer>
+        </div>
+      </div>
+    </footer>
       );
     }
 
